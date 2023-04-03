4742fcd377bd9f50aca45ca566cd83bea9060a7f
kim-svedberg <110769011+kim-svedberg@users.noreply.github.com>
2023-04-03 15:50:40 +0100

    Enemy can spot the player if they are in front of it.
    - Front facing the enemy = debug message that you've been spotted
    - Standing behind the enemy won't get you spotted. Might change this though, so that the enemy will notice you if you're simply within a distance.
    - Need to fix: Obstacles (like walls) are not blocking the enemy vision.

---
+++ Assets/Scripts/EnemyDetection.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDetection : MonoBehaviour
+{
+    public float viewRadius;
+    public float viewAngle;
+    
+    //Player layer
+    public LayerMask targetPlayer; 
+    public LayerMask obstacleMask;
+
+    public GameObject player; 
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        //Responsible for angle of player & enemy
+        Vector3 playerTarget = (player.transform.position - transform.position).normalized;
+        
+        //Checks if enemies forward position & playertarget is less than the viewing angle / 2. Divide by 2 to create 45 degrees towards
+        //both left and right.
+        if (Vector3.Angle(transform.forward, playerTarget) < viewAngle / 2) 
+        {
+            float distanceToTarget = Vector3.Distance(transform.position, player.transform.position);
+            if(distanceToTarget <= viewRadius)
+            {
+                //If there is no obstacle in the way = enemy has detected the player. 
+                if(Physics.Raycast(transform.position, playerTarget, distanceToTarget, obstacleMask) == false )
+                {
+                    Debug.Log("You have been spotted.");
+                }
+            }
+        }
+    }
+}
