15118e8cb8f3e1a189ae67f64c9072c1bf9a4e86
Frej Wikström <frejoskarwikstrom@gmail.com>
2023-05-07 04:31:12 +0200

    Imported text mesh pro and added ammo counter

---
+++ Assets/AmmoDisplayer.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+[RequireComponent(typeof(TextMeshProUGUI))]
+public class AmmoDisplayer : MonoBehaviour
+{
+    [SerializeField]
+    private PlayerGunSelector gunSelector;
+    private TextMeshProUGUI ammoText;
+
+    private void Awake()
+    {
+        ammoText = GetComponent<TextMeshProUGUI>();
+    }
+
+    private void Update()
+    {
+        ammoText.SetText(
+            $"{gunSelector.activeGun.ammoConfig.currentClipAmmo} / " +    
+            $"{gunSelector.activeGun.ammoConfig.currentAmmo}"
+        );
+    }
+}
