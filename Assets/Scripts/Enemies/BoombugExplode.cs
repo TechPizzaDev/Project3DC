6658d3e460837f4f18ee021378a3f2c470414ac3
Elovsson <martin_c_elovsson@hotmail.com>
2023-05-09 19:48:41 +0200

    Adding explosion effect to Boombug
    Co-Authored-By: kim-svedberg <110769011+kim-svedberg@users.noreply.github.com>

--- Assets/Scripts/Enemies/BoombugExplode.cs
+++ Assets/Scripts/Enemies/BoombugExplode.cs
@@ -1,13 +1,16 @@
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class BoombugExplode : MonoBehaviour
 {
+    [SerializeField] GameObject explosionFX;
     public int explosionDamage = 45;
     public float damageRange = 3;
     
     EnemyDetection enemyDetection;
 
     [SerializeField] float timer = 1f;
+    float explosionFXTimer = 1f;
     public bool explosionMode;
 
     void Start()
@@ -28,6 +31,9 @@ public class BoombugExplode : MonoBehaviour
             float distanceToPlayer = Vector3.Distance(transform.position, target.position);
 
             Debug.Log("Boom!");
+
+            Instantiate(explosionFX, transform.position, Quaternion.identity); //Quaternion.identity = no rotation
+
             if (distanceToPlayer <= damageRange)
             {
                 if (target.TryGetComponent(out IDamageable damageable))
@@ -35,6 +41,7 @@ public class BoombugExplode : MonoBehaviour
                     damageable.TakeDamage(explosionDamage);
                 }
             }
+
             Destroy(gameObject);
         }
     }
