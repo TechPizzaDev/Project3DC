857b22fbef0fd463df8effe91f93a28384bcd8ab
kim-svedberg <kimastridveronika@gmail.com>
2023-05-09 20:42:16 +0200

    Enemy detection when shooting the enemy fixed
    Co-Authored-By: Martin Elovsson <112435323+Elovsson@users.noreply.github.com>

--- Assets/Guns/Scripts/Gun.cs
+++ Assets/Guns/Scripts/Gun.cs
@@ -230,8 +230,10 @@ public class Gun : ScriptableObject, ICloneable
 
         if (hitCollider.TryGetComponent(out IDamageable damageable))
         {
+            
             Debug.Log("Hit Damageable");
             damageable.TakeDamage(damageConfig.GetDamage(distanceTraveled));
+            damageable.Detection();
         }
     }
 
