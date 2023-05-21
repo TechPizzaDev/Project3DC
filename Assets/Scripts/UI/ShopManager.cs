81905a2f1559974a19d367e10df7bb1a82497933
kim-svedberg <110769011+kim-svedberg@users.noreply.github.com>
2023-05-21 16:10:11 +0200

    Easily display items on the shop templates through scriptable objects

--- Assets/Scripts/UI/ShopManager.cs
+++ Assets/Scripts/UI/ShopManager.cs
@@ -8,13 +8,24 @@ public class ShopManager : MonoBehaviour
     //Just temporary number. Should obviously be replaced by the Player's actual dollar/health count.
     public int dollars = 1000;
     public int health = 80;
+
     public TMP_Text dollarUI;
     public TMP_Text healthUI;
+
+    public ShopItem[] shopItem;
+    public ShopTemplate[] shopTemplate;
+    public GameObject[] shopTemplateGO;
 
     // Start is called before the first frame update
     void Start()
     {
+        for(int i = 0; i < shopItem.Length; i++)
+        {
+            shopTemplateGO[i].SetActive(true);
+        }
+
         UIDisplay();
+        LoadTemplates();
     }
 
     // Update is called once per frame
@@ -28,4 +39,17 @@ public class ShopManager : MonoBehaviour
         dollarUI.text = "$ " + dollars;
         healthUI.text = "HP " + health;
     }
+
+    public void LoadTemplates()
+    {
+        for(int i = 0; i < shopItem.Length; i++)
+        {
+            shopTemplate[i].titleTxt.text = shopItem[i].title;
+            shopTemplate[i].descriptionTxt.text = shopItem[i].description;
+            shopTemplate[i].priceTxt.text = "$ " + shopItem[i].baseCost.ToString();
+
+        }
+    }
+
+
 }
